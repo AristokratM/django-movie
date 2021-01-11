134af081168687b5d8c550e422897220718e2fa0
AristokratM <61250102+AristokratM@users.noreply.github.com>
2021-01-12 00:08:04 +0200

    Reviews

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import DetailView, ListView
 from .models import Movie
+from .forms import ReviewForm
 # Create your views here.
 
 
@@ -13,4 +14,16 @@ class MoviesView(ListView):
 class MovieDetailView(DetailView):
     """Детельна інформація про фільм"""
     queryset = Movie.objects.filter(draft=False)
+
+
+class AddReview(View):
+    """Додати відгук"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(pk=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolut_url())
 
